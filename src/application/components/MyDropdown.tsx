4edadce869548bd783836e712f6677152e310c26
BoJoNVi <bonjomontes@gmail.com>
2023-08-10 13:36:29 +0800

    feat: custom dropdown

---
+++ src/application/components/MyDropdown.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+type Props = {
+  button: React.ReactNode;
+  children: React.ReactNode;
+};
+
+function MyDropdown({ button, children }: Props) {
+  const ref = React.useRef<HTMLDivElement>(null);
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <div className="relative">
+      <button
+        onBlur={() => {
+          if (!ref.current?.matches(":hover")) {
+            setOpen(false);
+          }
+        }}
+        className={`${open ? "bg-blue-100" : ""} rounded-md hover:bg-blue-100`}
+        onClick={() => {
+          setOpen((prev) => !prev);
+        }}
+      >
+        {button}
+      </button>
+      <div
+        ref={ref}
+        onMouseLeave={() => {
+          setOpen(false);
+        }}
+        className={`absolute grid duration-300 ${
+          open ? "grid-rows-[1fr]" : "grid-rows-[0fr]"
+        } shadow min-w-full top-full mt-1 bg-white`}
+      >
+        <div className="overflow-hidden">{children}</div>
+      </div>
+    </div>
+  );
+}
+
+export default MyDropdown;
