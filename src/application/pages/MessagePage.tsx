a810fe479cc7b5f950bf75d65957944a2a9d4502
BoJoNVi <bonjomontes@gmail.com>
2023-08-10 16:52:32 +0800

    feat: message page table

--- src/application/pages/MessagePage.tsx
+++ src/application/pages/MessagePage.tsx
@@ -1,5 +1,27 @@
+import { messageGetAll } from "../../domain/services/messageService";
+import DynamicTable from "../components/DynamicTable";
+
 function MessagePage() {
-  return <div>MessagePage</div>;
+  return (
+    <section className="p-4">
+      {/* First section */}
+      <section className="rounded-lg overflow-hidden shadow border">
+        {/* Header */}
+        <header className="p-2 flex items-center">
+          <h2 className="text-xl font-medium mr-auto">Messages</h2>
+
+          <button className="px-4 py-2 bg-blue-500 text-white rounded-md transition hover:bg-blue-400">
+            Add +
+          </button>
+        </header>
+
+        {/* Table */}
+        <section>
+          <DynamicTable getData={messageGetAll} />
+        </section>
+      </section>
+    </section>
+  );
 }
 
 export default MessagePage;
