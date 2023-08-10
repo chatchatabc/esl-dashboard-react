287f3c6aa163fc28b4ac4ffff2ac6fd30c954dde
BoJoNVi <bonjomontes@gmail.com>
2023-08-10 13:49:52 +0800

    feat: sidebar layout

---
+++ src/application/components/Sidebar.tsx
@@ -0,0 +1,51 @@
+import { useLocation } from "react-router-dom";
+import HomeIcon from "../assets/HomeIcon";
+import MessageIcon from "../assets/MessageIcon";
+import UserIcon from "../assets/UserIcon";
+
+function Sidebar() {
+  const location = useLocation();
+  const { pathname } = location;
+
+  const navLinks = [
+    {
+      label: "Home",
+      icon: <HomeIcon />,
+      href: "/",
+    },
+    {
+      label: "Users",
+      icon: <UserIcon />,
+      href: "/users",
+    },
+    {
+      label: "Messages",
+      icon: <MessageIcon />,
+      href: "/messages",
+    },
+  ];
+
+  return (
+    <nav>
+      <ul className="p-0.5">
+        {navLinks.map((navLink) => {
+          return (
+            <li key={navLink.href} className="p-0.5">
+              <a
+                href={navLink.href}
+                className={`flex rounded-md items-center ${
+                  pathname === navLink.href ? "bg-blue-200" : ""
+                } transition hover:bg-blue-200`}
+              >
+                <span className="mr-2 w-10 h-10 p-1">{navLink.icon}</span>
+                <span>{navLink.label}</span>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
+
+export default Sidebar;
