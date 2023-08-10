3b195b5008187e241e97651902611e42b9a21324
BoJoNVi <bonjomontes@gmail.com>
2023-08-10 13:51:00 +0800

    chore: added icons for home and message

---
+++ src/application/assets/HomeIcon.tsx
@@ -0,0 +1,13 @@
+function HomeIcon() {
+  return (
+    <svg
+      className="w-full h-full"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path fill="currentColor" d="M4 21V9l8-6l8 6v12h-6v-7h-4v7H4Z" />
+    </svg>
+  );
+}
+
+export default HomeIcon;
