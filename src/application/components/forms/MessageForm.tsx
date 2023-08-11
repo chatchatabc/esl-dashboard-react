45c2fc0d14ea0bf98beab3d1992a4c786bece9d3
BoJoNVi <bonjomontes@gmail.com>
2023-08-11 09:36:38 +0800

    feat: simple form for sending message

---
+++ src/application/components/forms/MessageForm.tsx
@@ -0,0 +1,114 @@
+import { Button, Form, FormInstance, Input, Select } from "antd";
+import { messageCreate } from "../../../domain/services/messageService";
+import React from "react";
+import { userGetAll } from "../../../domain/services/userService";
+import { User } from "../../../../../esl-workers/src/domain/models/UserModel";
+import { messageTemplateGetAll } from "../../../domain/services/messageTemplateService";
+import { MessageTemplate } from "../../../../../esl-workers/src/domain/models/MessageModel";
+
+type Props = {
+  loading: boolean;
+  handleSubmit: (
+    action: (params: any) => Promise<any>,
+    values: any,
+    success: string,
+    fail: string
+  ) => Promise<any>;
+  formRef: FormInstance;
+};
+
+function MessageForm({ loading, handleSubmit, formRef }: Props) {
+  const [localLoading, setLocalLoading] = React.useState(true);
+  const [users, setUsers] = React.useState<User[]>([]);
+  const [templates, setTemplates] = React.useState<MessageTemplate[]>([]);
+
+  React.useEffect(() => {
+    if (localLoading) {
+      (async () => {
+        const responseUser = await userGetAll({});
+        if (responseUser) {
+          setUsers(responseUser.content);
+        }
+
+        const responseTemplate = await messageTemplateGetAll({});
+        if (responseTemplate) {
+          setTemplates(responseTemplate.content);
+        }
+      })();
+
+      setLocalLoading(false);
+    }
+  }, []);
+
+  return (
+    <Form
+      layout="vertical"
+      form={formRef}
+      onFinish={(e) => {
+        handleSubmit(messageCreate, e, "Success", "Fail");
+      }}
+    >
+      <Form.Item
+        rules={[
+          {
+            required: true,
+            message: "Need some input here",
+          },
+        ]}
+        name="receiverId"
+        label="Receiver"
+      >
+        <Select
+          placeholder="Select a receiver"
+          options={users.map((user) => {
+            return { label: user.username, value: user.id };
+          })}
+        />
+      </Form.Item>
+
+      <Form.Item
+        rules={[
+          {
+            required: true,
+            message: "Need some input here",
+          },
+        ]}
+        name="title"
+        label="Title"
+      >
+        <Input placeholder="Title" />
+      </Form.Item>
+
+      <Form.Item name="messageTemplate" label="Message Template">
+        <Select
+          placeholder="Select a message template"
+          onChange={(e) => {
+            formRef.setFieldsValue({ message: e });
+          }}
+          options={templates.map((template) => {
+            return { label: template.title, value: template.message };
+          })}
+        />
+      </Form.Item>
+
+      <Form.Item
+        rules={[
+          {
+            required: true,
+            message: "Need some input here",
+          },
+        ]}
+        name="message"
+        label="Message"
+      >
+        <Input.TextArea placeholder="Select a message template" />
+      </Form.Item>
+
+      <Form.Item hidden>
+        <Button htmlType="submit" loading={loading}></Button>
+      </Form.Item>
+    </Form>
+  );
+}
+
+export default MessageForm;
--- src/application/pages/MessagePage.tsx
+++ src/application/pages/MessagePage.tsx
@@ -1,7 +1,11 @@
 import { messageGetAll } from "../../domain/services/messageService";
 import DynamicTable from "../components/DynamicTable";
+import { modalUpdate } from "../redux/features/modalSlice";
+import { useAppDispatch } from "../redux/hooks";
 
 function MessagePage() {
+  const dispatch = useAppDispatch();
+
   return (
     <section className="p-4">
       {/* First section */}
@@ -10,7 +14,18 @@ function MessagePage() {
         <header className="p-2 flex items-center">
           <h2 className="text-xl font-medium mr-auto">Messages</h2>
 
-          <button className="px-4 py-2 bg-blue-500 text-white rounded-md transition hover:bg-blue-400">
+          <button
+            onClick={() => {
+              dispatch(
+                modalUpdate({
+                  show: true,
+                  content: "message",
+                  title: "Add Message",
+                })
+              );
+            }}
+            className="px-4 py-2 bg-blue-500 text-white rounded-md transition hover:bg-blue-400"
+          >
             Add +
           </button>
         </header>
