61ee4eb64ef72dbf789a27deb5bf8b4f0bcc192e
BoJoNVi <bonjomontes@gmail.com>
2023-08-11 08:51:19 +0800

    fix: no react redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import RouteManager from "./application/RouteManager";
+import { Provider } from "react-redux";
+import store from "./application/redux/store";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<RouteManager />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <RouteManager />
+  </Provider>
+);
