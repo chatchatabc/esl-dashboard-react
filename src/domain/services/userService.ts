2105230d533673e051bb62437eb9d4e9f73e7610
BoJoNVi <bonjomontes@gmail.com>
2023-08-14 14:38:01 +0800

    feat: service for creating user

--- src/domain/services/userService.ts
+++ src/domain/services/userService.ts
@@ -1,3 +1,4 @@
+import { UserCreateInput } from "../../../../esl-workers/src/domain/models/UserModel";
 import { trpcClient } from "../infras/trpcActions";
 
 export async function userGetAll(params: { page?: number; size?: number }) {
@@ -7,5 +8,15 @@ export async function userGetAll(params: { page?: number; size?: number }) {
   } catch (e) {
     console.log(e);
     return undefined;
+  }
+}
+
+export async function userCreate(params: UserCreateInput) {
+  try {
+    const res = await trpcClient.user.create.mutate(params);
+    return res;
+  } catch (e) {
+    console.log(e);
+    return false;
   }
 }
