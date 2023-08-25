a5db2d117b787be79a7c2ee47ce692bba81ee8f1
BoJoNVi <bonjomontes@gmail.com>
2023-08-25 17:20:26 +0800

    feat: UI for updating teacher course info

--- src/application/components/tables/TeacherCourseTable.tsx
+++ src/application/components/tables/TeacherCourseTable.tsx
@@ -3,12 +3,16 @@ import { ColumnsType } from "antd/es/table";
 import { utilFormatCurrency } from "../../../domain/services/utilService";
 import { Course } from "../../../../../esl-workers/src/domain/models/CourseModel";
 import { courseGetAll } from "../../../domain/services/courseService";
+import { useAppDispatch } from "../../redux/hooks";
+import { modalUpdate } from "../../redux/features/modalSlice";
 
 type Props = {
   teacherId: number;
 };
 
 function TeacherCourseTable({ teacherId }: Props) {
+  const dispatch = useAppDispatch();
+
   const columns: ColumnsType<Course> = [
     {
       key: "name",
@@ -25,6 +29,31 @@ function TeacherCourseTable({ teacherId }: Props) {
       title: "Price",
       render: (record: Course) => {
         return <p key={record.price}>{utilFormatCurrency(record.price)}</p>;
+      },
+    },
+    {
+      key: "action",
+      title: "Actions",
+      render: (record: Course) => {
+        return (
+          <div>
+            <button
+              className="text-blue-500"
+              onClick={() => {
+                dispatch(
+                  modalUpdate({
+                    show: true,
+                    title: "Edit Course",
+                    content: "course",
+                    data: record,
+                  })
+                );
+              }}
+            >
+              Edit
+            </button>
+          </div>
+        );
       },
     },
   ];
--- src/domain/services/courseService.ts
+++ src/domain/services/courseService.ts
@@ -1,6 +1,7 @@
 import {
   CourseCreateInput,
   CoursePagination,
+  CourseUpdateInput,
 } from "../../../../esl-workers/src/domain/models/CourseModel";
 import { trpcClient } from "../infras/trpcActions";
 
@@ -17,6 +18,16 @@ export async function courseGetAll(params: CoursePagination) {
 export async function courseCreate(params: CourseCreateInput) {
   try {
     const res = await trpcClient.course.create.mutate(params);
+    return res;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+}
+
+export async function courseUpdate(params: CourseUpdateInput) {
+  try {
+    const res = await trpcClient.course.update.mutate(params);
     return res;
   } catch (e) {
     console.log(e);
