d4d40f4b48ace86d03376e4631b330369a237292
BoJoNVi <bonjomontes@gmail.com>
2023-09-11 16:04:01 +0800

    refactor: way of initializing trpc client

--- src/domain/infras/trpcActions.ts
+++ src/domain/infras/trpcActions.ts
@@ -1,20 +1,4 @@
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import { createTRPCReact } from "@trpc/react-query";
 import type { TrpcRouter } from "../../../../esl-workers/src/application/trpc/index";
 
-export const trpcClient = createTRPCProxyClient<TrpcRouter>({
-  links: [
-    httpBatchLink({
-      url: "http://localhost:8787/trpc",
-      // url: "https://esl-trpc.bonjomontes.workers.dev/trpc",
-      async headers() {
-        return {};
-      },
-      fetch(url, options) {
-        return fetch(url, {
-          ...options,
-          credentials: "include",
-        });
-      },
-    }),
-  ],
-});
+export const trpc = createTRPCReact<TrpcRouter>();
