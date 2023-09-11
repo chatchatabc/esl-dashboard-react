566bdb4f68a7dc8d7aba3de4162adc3d0a2dcfad
BoJoNVi <bonjomontes@gmail.com>
2023-09-11 14:27:42 +0800

    build: integrated react query

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider as ReduxProvider } from "react-redux";
+import store from "./application/redux/store";
+import React from "react";
+import RouteManager from "./application/RouteManager";
+import { trpc } from "./domain/infras/trpcActions";
+import { httpBatchLink } from "@trpc/client";
+
+function App() {
+  const [queryClient] = React.useState(() => {
+    return new QueryClient();
+  });
+  const [trpcClient] = React.useState(() => {
+    return trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: "http://localhost:8787/trpc",
+          // url: "https://esl-trpc.bonjomontes.workers.dev/trpc",
+          async headers() {
+            return {};
+          },
+          fetch(url, options) {
+            return fetch(url, {
+              ...options,
+              credentials: "include",
+            });
+          },
+        }),
+      ],
+    });
+  });
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <ReduxProvider store={store}>
+          <RouteManager queryClient={queryClient} />
+        </ReduxProvider>
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import RouteManager from "./application/RouteManager";
-import { Provider } from "react-redux";
-import store from "./application/redux/store";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <RouteManager />
-  </Provider>
+  <App />
 );
