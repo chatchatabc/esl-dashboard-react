8ea5894353c0c9d094232bf910ba0d6610252571
BoJoNVi <bonjomontes@gmail.com>
2023-09-18 15:22:03 +0800

    improv: login page
    added loading states and notifications for feedback

--- src/application/pages/LoginPage.tsx
+++ src/application/pages/LoginPage.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import { authGetUserId, authLogin } from "../../domain/services/authService";
 import { Navigate, useNavigate } from "react-router-dom";
+import { Button, Form, Input, message } from "antd";
 
 function LoginPage() {
+  const [loading, setLoading] = React.useState(false);
   const navigate = useNavigate();
 
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
+  async function handleSubmit(e: Record<string, any>) {
+    setLoading(true);
 
-    const objData = Object.fromEntries(new FormData(e.currentTarget));
-    const data = {
-      username: objData.username as string,
-      password: objData.password as string,
-    };
-    const response = await authLogin(data);
+    const response = await authLogin(e as any);
 
     if (!response) {
-      alert("Invalid username or password");
+      message.error("Failed to login.");
+      setLoading(false);
     } else {
+      message.success("Successfully logged in.");
       navigate("/");
     }
   }
@@ -28,52 +27,58 @@ function LoginPage() {
 
   return (
     <div className="flex-1 flex items-center justify-center">
-      <form
-        onSubmit={handleSubmit}
-        className="max-w-sm w-full mx-auto p-8 border shadow-xl"
+      <Form
+        layout="vertical"
+        onFinish={handleSubmit}
+        className="max-w-lg w-full bg-blue-500 bg-opacity-10 mx-auto p-8 border shadow-xl rounded-3xl"
       >
         <header>
           <h1 className="text-2xl font-bold">Sign in</h1>
         </header>
 
-        <section className="mt-4 space-y-2">
-          <div>
-            <label htmlFor="username" className="flex flex-col">
-              <span className="text-xs font-bold">Username</span>
-              <input
-                required
-                className="p-2 border rounded-md"
-                id="username"
-                placeholder="Username"
-                name="username"
-              />
+        <section className="mt-4 [&>*]:my-2">
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Need some input here",
+              },
+            ]}
+          >
+            <label>
+              <p className="text-xs font-bold">Username</p>
+              <Input className="py-2" placeholder="Username" />
             </label>
-          </div>
+          </Form.Item>
 
-          <div>
-            <label htmlFor="password" className="flex flex-col">
-              <span className="text-xs font-bold">Password</span>
-              <input
-                type="password"
-                required
-                className="p-2 border rounded-md"
-                id="password"
-                placeholder="Password"
-                name="password"
-              />
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Need some input here",
+              },
+            ]}
+          >
+            <label>
+              <p className="text-xs font-bold">Password</p>
+              <Input.Password className="py-2" placeholder="Password" />
             </label>
-          </div>
+          </Form.Item>
         </section>
 
         <footer className="mt-4 flex space-x-2">
-          <button className="px-3 py-1 rounded-md bg-blue-500 text-white">
+          <Button
+            htmlType="submit"
+            size="large"
+            loading={loading}
+            className="px-3 py-1 rounded-md bg-blue-500 text-white"
+          >
             Login
-          </button>
-          {/* <button type="button" className="px-3 py-1 text-blue-500">
-            Register
-          </button> */}
+          </Button>
         </footer>
-      </form>
+      </Form>
     </div>
   );
 }
