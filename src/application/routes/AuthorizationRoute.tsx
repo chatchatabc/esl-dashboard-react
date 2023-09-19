fb298f73fa05e15914726165fd95aed585262df1
BoJoNVi <bonjomontes@gmail.com>
2023-09-19 14:42:19 +0800

    feat: authorization route to protect routes from unauthorized roles

---
+++ src/application/routes/AuthorizationRoute.tsx
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { authGetProfile } from "../../domain/services/authService";
+import NotFoundPage from "../pages/NotFoundPage";
+import { Outlet } from "react-router-dom";
+
+type Props = {
+  allowedRoles: number[];
+  children?: React.ReactNode;
+};
+
+function AuthorizationRoute({ allowedRoles, children }: Props) {
+  const userQuery = useQuery({
+    queryKey: ["users", "profile"],
+    queryFn: async () => {
+      const data = await authGetProfile();
+      return data;
+    },
+  });
+
+  if (userQuery.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (allowedRoles.includes(userQuery.data?.roleId ?? 0)) {
+    return <div>{children ?? <Outlet />}</div>;
+  }
+
+  return <NotFoundPage />;
+}
+
+export default AuthorizationRoute;
