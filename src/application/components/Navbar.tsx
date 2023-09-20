8e654fb6b2dbfb6170d46e63f6b77d958ad2667a
BoJoNVi <bonjomontes@gmail.com>
2023-09-20 10:07:35 +0800

    improv: navbar
    -Changed the title from ESL Admin Dashboard to Dashboard
    -Show user alias on the right side

--- src/application/components/Navbar.tsx
+++ src/application/components/Navbar.tsx
@@ -1,25 +1,39 @@
 import { useNavigate } from "react-router-dom";
-import { authLogout } from "../../domain/services/authService";
+import { authGetProfile, authLogout } from "../../domain/services/authService";
 import UserIcon from "../assets/UserIcon";
 import MyDropdown from "./MyDropdown";
+import { useQuery } from "@tanstack/react-query";
 
 function Navbar() {
   const navigate = useNavigate();
+  const userQuery = useQuery({
+    queryKey: ["users", "profile"],
+    queryFn: async () => {
+      const res = await authGetProfile();
+      return res;
+    },
+  });
 
   return (
     <section className="px-4 py-2 flex items-center">
       {/* Title */}
-      <h2 className="text-2xl font-bold">ESL Admin Dashboard</h2>
+      <h2 className="text-2xl font-bold">Dashboard</h2>
 
       {/* Menu */}
       <div className="ml-auto">
         <MyDropdown
           button={
             <div className="space-x-2 flex items-center pl-2 rounded-md transition hover:bg-blue-100">
-              <p>Welcome User!</p>
-              <div className="w-10 h-10 p-1 rounded-full">
-                <UserIcon />
-              </div>
+              {userQuery.isLoading ? (
+                <div className="w-10 h-10 border-r-2 rounded-full border-black animate-spin border-t-2"></div>
+              ) : (
+                <>
+                  <p>Welcome {userQuery.data?.alias}!</p>
+                  <div className="w-10 h-10 p-1 rounded-full">
+                    <UserIcon />
+                  </div>
+                </>
+              )}
             </div>
           }
         >
