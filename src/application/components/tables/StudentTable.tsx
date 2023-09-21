e4379189f5eab7e378c838cd60ca3f38d87146f3
BoJoNVi <bonjomontes@gmail.com>
2023-09-21 14:20:32 +0800

    refactor: students table

--- src/application/components/tables/StudentTable.tsx
+++ src/application/components/tables/StudentTable.tsx
@@ -1,8 +1,6 @@
 import Table, { ColumnsType, TableProps } from "antd/es/table";
 import { utilFormatDateAndTime } from "../../../domain/services/utilService";
 import { useNavigate } from "react-router-dom";
-import { useAppDispatch } from "../../stores/hooks";
-import { modalUpdate } from "../../stores/app/modalSlice";
 import { CommonContent } from "../../../../../esl-backend-workers/src/domain/models/CommonModel";
 import { Student } from "../../../../../esl-backend-workers/src/domain/models/StudentModel";
 
@@ -12,13 +10,12 @@ type Props = TableProps<any> & {
 
 function StudentTable({ data, ...props }: Props) {
   const navigate = useNavigate();
-  const dispatch = useAppDispatch();
 
   const columns: ColumnsType<Student> = [
     {
       title: "Student Name",
       key: "alias",
-      render: (record: Student) => {
+      render: (_, record) => {
         return (
           <button
             onClick={() => {
@@ -32,25 +29,25 @@ function StudentTable({ data, ...props }: Props) {
       },
     },
     {
-      title: "Username",
-      key: "username",
-      render: (record: Student) => {
-        return (
-          <button
-            onClick={() => {
-              navigate("/users/" + record.user?.username);
-            }}
-            className="text-blue-500 underline hover:no-underline"
-          >
-            {record.user?.username}
-          </button>
-        );
+      title: "Phone Number",
+      key: "phoneNumber",
+      dataIndex: "user",
+      render: (_, data) => {
+        return <p>{data.user.phone ?? "NULL"}</p>;
+      },
+    },
+    {
+      title: "Credits",
+      key: "credits",
+      dataIndex: "user",
+      render: (_, data) => {
+        return <p>{data.user.credits}</p>;
       },
     },
     {
       title: "Created At",
       key: "createdAt",
-      render: (record: Student) => {
+      render: (_, record) => {
         return (
           <p>{utilFormatDateAndTime("en-US", new Date(record.createdAt))}</p>
         );
@@ -59,34 +56,9 @@ function StudentTable({ data, ...props }: Props) {
     {
       title: "Last Updated",
       key: "updatedAt",
-      render: (record: Student) => {
+      render: (_, record) => {
         return (
           <p>{utilFormatDateAndTime("en-US", new Date(record.updatedAt))}</p>
-        );
-      },
-    },
-    {
-      title: "Actions",
-      key: "actions",
-      render: (record: Student) => {
-        return (
-          <div>
-            <button
-              onClick={() => {
-                dispatch(
-                  modalUpdate({
-                    show: true,
-                    title: "Edit Teacher",
-                    content: "teacher",
-                    data: record,
-                  })
-                );
-              }}
-              className="text-blue-500 underline hover:no-underline"
-            >
-              Edit
-            </button>
-          </div>
         );
       },
     },
