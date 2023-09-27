7fa9a019f83127d1fbec37b397ff96ebd6da7737
BoJoNVi <bonjomontes@gmail.com>
2023-09-27 11:03:36 +0800

    refactor: students table

--- src/application/components/tables/StudentTable.tsx
+++ src/application/components/tables/StudentTable.tsx
@@ -1,8 +1,15 @@
 import Table, { ColumnsType, TableProps } from "antd/es/table";
-import { utilFormatDateAndTime } from "../../../domain/services/utilService";
 import { useNavigate } from "react-router-dom";
 import { CommonContent } from "../../../../../esl-backend-workers/src/domain/models/CommonModel";
 import { Student } from "../../../../../esl-backend-workers/src/domain/models/StudentModel";
+import { useAppDispatch } from "../../stores/hooks";
+import { modalUpdate } from "../../stores/app/modalSlice";
+import { Modal, message } from "antd";
+import {
+  userRevokePhoneVerification,
+  userVerifyPhone,
+} from "../../../domain/services/userService";
+import { useQueryClient } from "@tanstack/react-query";
 
 type Props = TableProps<any> & {
   data?: CommonContent<Student> | null;
@@ -10,30 +17,106 @@ type Props = TableProps<any> & {
 
 function StudentTable({ data, ...props }: Props) {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const queryClient = useQueryClient();
 
   const columns: ColumnsType<Student> = [
     {
-      title: "Student Name",
-      key: "alias",
+      key: "username",
+      title: "Username",
+      fixed: "left",
+      width: 200,
       render: (_, record) => {
         return (
           <button
             onClick={() => {
-              navigate("/students/" + record.user?.username);
+              navigate("/students/" + record.user.username);
             }}
             className="text-blue-500 underline hover:no-underline"
           >
-            {record.alias}
+            {record.user.username}
           </button>
         );
       },
     },
     {
+      title: "Student Name",
+      key: "alias",
+      dataIndex: "alias",
+    },
+    {
+      key: "credit",
+      title: "Credits",
+      render: (_, record) => {
+        return (
+          <button
+            onClick={() => {
+              dispatch(
+                modalUpdate({
+                  show: true,
+                  content: "credit",
+                  title: "Add Credit",
+                  data: { userId: record.userId },
+                })
+              );
+            }}
+            className="underline hover:no-underline"
+          >
+            {record.user.credits}点
+          </button>
+        );
+      },
+    },
+    {
+      key: "phone",
       title: "Phone Number",
-      key: "phoneNumber",
-      dataIndex: "user",
-      render: (_, data) => {
-        return <p>{data.user.phone ?? "NULL"}</p>;
+      render: (_, record) => {
+        console.log(record);
+        return (
+          <button
+            onClick={() => {
+              Modal.confirm({
+                title: `${
+                  record.user.phoneVerifiedAt ? "Revoke" : "Verify"
+                } Phone verification`,
+                content: `Are you sure you want to ${
+                  record.user.phoneVerifiedAt ? "revoke" : "verify"
+                } phone verification?`,
+                onOk: async () => {
+                  let res: any;
+                  if (record.user.phoneVerifiedAt) {
+                    res = await userRevokePhoneVerification({
+                      id: record.userId,
+                    });
+                  } else {
+                    res = await userVerifyPhone({ id: record.userId });
+                  }
+
+                  if (!res) {
+                    message.error("Something went wrong");
+                  } else {
+                    message.success("Success");
+                    queryClient.invalidateQueries(["students"]);
+                  }
+                },
+                okButtonProps: {
+                  className: `${
+                    record.user.phoneVerifiedAt
+                      ? "bg-red-500 hover:bg-red-400"
+                      : "bg-green-500 hover:bg-green-400"
+                  } text-white rounded-md transition`,
+                },
+                okText: "Yes",
+                maskClosable: true,
+              });
+            }}
+            className={`underline ${
+              record.user.phoneVerifiedAt ? "text-green-500" : "text-red-500"
+            } hover:no-underline`}
+          >
+            {record.user.phone}
+          </button>
+        );
       },
     },
     {
@@ -42,24 +125,6 @@ function StudentTable({ data, ...props }: Props) {
       dataIndex: "user",
       render: (_, data) => {
         return <p>{data.user.credits}</p>;
-      },
-    },
-    {
-      title: "Created At",
-      key: "createdAt",
-      render: (_, record) => {
-        return (
-          <p>{utilFormatDateAndTime("en-US", new Date(record.createdAt))}</p>
-        );
-      },
-    },
-    {
-      title: "Last Updated",
-      key: "updatedAt",
-      render: (_, record) => {
-        return (
-          <p>{utilFormatDateAndTime("en-US", new Date(record.updatedAt))}</p>
-        );
       },
     },
   ];
