7192f1c3e437cb161ae9ab5a0c2ffdd8b0d6fc56
BoJoNVi <bonjomontes@gmail.com>
2023-09-27 09:24:02 +0800

    refactor: service for message

--- src/domain/services/messageService.ts
+++ src/domain/services/messageService.ts
@@ -9,23 +9,6 @@ export async function messageGetAll(params: { page?: number; size?: number }) {
   try {
     const res = await trpcClient.message.getAll.query(params);
 
-    if (res) {
-      const contentPromise = res.content.map(async (message) => {
-        if (message.userId) {
-          message.user = await trpcClient.user.get.query({
-            userId: message.userId,
-          });
-        }
-
-        message.messageTemplate = await trpcClient.messageTemplate.get.query({
-          messageTemplateId: message.messageTemplateId,
-        });
-
-        return message;
-      });
-      res.content = await Promise.all(contentPromise);
-    }
-
     return res;
   } catch (e) {
     console.log(e);
