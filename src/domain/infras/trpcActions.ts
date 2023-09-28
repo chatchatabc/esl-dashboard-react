33f9a589b68c902898fae97b556243acaa306aa9
BoJoNVi <bonjomontes@gmail.com>
2023-09-28 14:53:58 +0800

    build: change the dev backend port

--- src/domain/infras/trpcActions.ts
+++ src/domain/infras/trpcActions.ts
@@ -4,8 +4,9 @@ import type { TrpcRouter } from "../../../../esl-backend-workers/src/application
 export const trpcClient = createTRPCProxyClient<TrpcRouter>({
   links: [
     httpBatchLink({
-      url: "http://localhost:8787/trpc",
-      // url: "https://esl-trpc.bonjomontes.workers.dev/trpc",
+      url: "http://localhost:5000/trpc",
+      // url: "https://esl-trpc.bonjomontes.workers.dev/trpc", // Production
+      // url: "https://esl-trpc-test.bonjomontes.workers.dev/trpc", // Test
       async headers() {
         return {};
       },
