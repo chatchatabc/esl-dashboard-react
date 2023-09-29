77106afebbacdaadf42073c347ae6164aa7b792a
BoJoNVi <bonjomontes@gmail.com>
2023-09-29 13:16:35 +0800

    test: for login page

---
+++ src/tests/App.test.ts
@@ -0,0 +1,40 @@
+import puppeteer, { Browser, Page } from "puppeteer";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+let browser: Browser | null = null;
+let page: Page | null = null;
+
+describe("App.js", () => {
+  beforeAll(async () => {
+    browser = await puppeteer.launch({ headless: false });
+    page = await browser.newPage();
+  });
+
+  it("should be launched", async () => {
+    expect(browser).not.toBeNull();
+    expect(page).not.toBeNull();
+  });
+});
+
+describe("Login Page", () => {
+  it("should be redirected to the login page when not logged in", async () => {
+    await page?.goto("http://localhost:3000");
+    await page?.waitForSelector("h1");
+    const text = await page?.$eval("h1", (e) => e.textContent);
+    expect(text).toContain("Sign in");
+  });
+
+  it("should be able to login", async () => {
+    await page?.goto("http://localhost:3000");
+    await page?.waitForSelector("h1");
+    await page?.type("#username", "admin");
+    await page?.type("#password", "123456");
+    await page?.click("button");
+
+    await page?.waitForSelector(".home-page-message");
+    const text = await page?.$eval(".home-page-message", (e) => e.textContent);
+    expect(text).toContain("Welcome to ESL!");
+  });
+
+  afterAll(() => browser?.close());
+});
