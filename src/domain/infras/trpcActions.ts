e641058be4186c828b478ff60d4c241308da312c
BoJoNVi <bonjomontes@gmail.com>
2023-10-02 13:38:38 +0800

    refactor: added dev trpc endpoint

--- src/domain/infras/trpcActions.ts
+++ src/domain/infras/trpcActions.ts
@@ -4,7 +4,7 @@ import type { TrpcRouter } from "../../../../esl-backend-workers/src/application
 export const trpcClient = createTRPCProxyClient<TrpcRouter>({
   links: [
     httpBatchLink({
-      url: "http://localhost:5000/trpc",
+      url: "http://localhost:5000/trpc", // Dev
       // url: "https://esl-trpc.bonjomontes.workers.dev/trpc", // Production
       // url: "https://esl-trpc-test.bonjomontes.workers.dev/trpc", // Test
       async headers() {
