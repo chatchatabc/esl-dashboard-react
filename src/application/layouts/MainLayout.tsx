989d336f80ea811b283aa62d44a3a7af06d9506e
BoJoNVi <bonjomontes@gmail.com>
2023-10-04 14:24:28 +0800

    refactor: main layout divider color

--- src/application/layouts/MainLayout.tsx
+++ src/application/layouts/MainLayout.tsx
@@ -18,14 +18,14 @@ export function MainLayout() {
   }
 
   return (
-    <div className="flex-1 flex bg-background-50 flex-col divide-y-2 divide-primary text-text">
+    <div className="flex-1 flex bg-background flex-col divide-y divide-primary text-text">
       {/* Header */}
       <header>
         <Navbar />
       </header>
 
       {/* Section */}
-      <section className="divide-x-2 divide-primary flex flex-1">
+      <section className="divide-x divide-primary flex flex-1">
         {/* Sidebar */}
         <aside className="min-w-[15vw]">
           <Sidebar />
